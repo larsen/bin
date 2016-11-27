13f9fd77c8d7d7b02557c781185e6342d77cdbfd
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-11-27 12:00:26 +0100

    touchpad toggle, libinput version

--- functions.sh
+++ functions.sh
@@ -6,3 +6,13 @@ function toggle_touchpad_synaptics() {
       synclient TouchpadOff=1
   fi
 }
+
+function toggle_touchpad_libinput() {
+  TOUCHPAD_DEVICE_NUMBER=$(xinput list | grep Touchpad | sed -nr 's/.*id=([0-9]+).*/\1/p')
+  TOUCHPAD_STATE=$(xinput list-props 11 | grep "Device Enabled" | cut -d: -f 2 | sed -nr 's/\s//gp')
+  if [ $TOUCHPAD_STATE == "1" ]; then
+      xinput --disable $TOUCHPAD_DEVICE_NUMBER
+  else
+      xinput --enable $TOUCHPAD_DEVICE_NUMBER
+  fi
+}
