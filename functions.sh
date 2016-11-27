166d43355e6623ca50c9eb8f928ec5e2a99c9bdc
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-11-27 11:59:59 +0100

    oops, forgot to delete part of the code

--- functions.sh
+++ functions.sh
@@ -1,6 +1,3 @@
-function toggle_touchpad() {
-  TOUCHPAD_OFF_STATE=$(synclient -l | grep TouchpadOff | cut -d '=' -f 2)
-  if [ $TOUCHPAD_OFF_STATE == "1" ]; then
 function toggle_touchpad_synaptics() {
   TOUCHPAD_STATE=$(synclient -l | grep TouchpadOff | cut -d '=' -f 2)
   if [ $TOUCHPAD_STATE == "1" ]; then
