7e95da5acb3065050a99a41770007bfc6072205c
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2018-04-08 11:01:45 +0200

    dired alias

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -4,6 +4,7 @@ alias i='toggle_screen_reverse_colors'
 alias e='emacsclient -t'
 alias ec='emacsclient -c'
 alias magit='emacsclient -c -e "(magit-status)"'
+alias dired='emacsclient -c -e "(my-dired-frame default-directory)"'
 
 alias h='hledger register --monthly --depth 1'
 alias hs='hledger register --monthly expe --depth 1'
