4d9c311138ea6c2bd34e11d2d63feb99841529e4
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-08-02 10:14:04 +0200

    configured PS2

--- init-personal-env.sh
+++ init-personal-env.sh
@@ -6,4 +6,6 @@
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="\u@\h \[\033[32m\]\W\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS2="…> "
+# export PS2="\u@\h \[\033[32m\]\W\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
