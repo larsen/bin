aa39a7a780e20c9b82be3854ff0b3921784e7b14
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-09-01 10:37:14 +0200

    download astronomy picture of the day

---
+++ apod.el
@@ -0,0 +1,35 @@
+#!/usr/local/bin/emacs --script
+
+(require 'package)
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d")
+
+(require 'larsen-functions)
+
+(defvar nasa-api-endpoint "https://api.nasa.gov")
+(defvar nasa-api-key (getenv "NASA_APIKEY"))
+(defvar apod-pictures-root "~/Pictures/apod")
+
+(defun apod ()
+  (get-json-data
+   (format "%s/planetary/apod?api_key=%s"
+           nasa-api-endpoint
+           nasa-api-key)))
+
+(defun ensure-directory-exists (dir)
+  (message (format "Creating directory %s" dir))
+  (condition-case err
+      (make-directory dir :create-parents)
+    (error (message (cadr err)))))
+
+(defun fetch-apod ()
+  (let* ((apod-payload (apod))
+         (date (cdr (assoc 'date apod-payload)))
+         (target-directory (format "%s/%s/" apod-pictures-root date))
+         (hdurl (cdr (assoc 'hdurl apod-payload)))
+         (explanation (cdr (assoc 'explanation apod-payload))))
+    (ensure-directory-exists target-directory)
+    (url-copy-file hdurl (expand-file-name (url-file-nondirectory hdurl)
+                                           target-directory))))
+
+(fetch-apod)
