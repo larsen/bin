d5b2d75f2658af4815f4770f03dd42c3303ac134
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-09-08 10:38:50 +0200

    lastest fixes

--- office-display-setup.sh
+++ office-display-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --auto \
-       --output HDMI-2 --left-of eDP-1 --mode 1920x1080 \
-       --output DP-1 --primary --left-of HDMI-2 --mode 1920x1080 
+xrandr --output HDMI-2 --left-of eDP-1 --mode 1920x1080 \
+       --output DP-1 --primary --left-of HDMI-2 --mode 1920x1080 \
+       --output eDP-1 --auto
