315a4dc8b1d956457a44d311c2a80ffe90858942
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-09-22 15:20:43 +0200

    optional command to pick a specific background picture

--- set-apod-wp.sh
+++ set-apod-wp.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 APOD_ROOT=~/Pictures/apod
-LASTEST_AVAILABLE_DIR=$(ls -1 -r $APOD_ROOT/ | head -n 1)
-LASTEST_AVAILABLE_IMAGE=$(ls -1 $APOD_ROOT/$LASTEST_AVAILABLE_DIR/*.jpg)
+
+case "$1" in
+	pick)
+		BACKGROUND_IMG_DIR=$(ls $APOD_ROOT | d)
+		;;
+	*)
+                BACKGROUND_IMG_DIR=$(ls -1 -r $APOD_ROOT/ | head -n 1)
+		;;
+esac
+
+LASTEST_AVAILABLE_IMAGE=$(ls -1 $APOD_ROOT/$BACKGROUND_IMG_DIR/*.jpg)
 
 feh --bg-fill $LASTEST_AVAILABLE_IMAGE
