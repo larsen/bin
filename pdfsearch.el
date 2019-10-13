f1c9ec55134cb2f2ee672e92d389034927f1f450
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-10-13 10:47:05 +0200

    some documentation

--- pdfsearch.el
+++ pdfsearch.el
@@ -12,12 +12,17 @@
   (error "You need to provide a pattern to search"))
 
 (defun all-pdf-files (path)
+  "Recursively find every PDF file starting from PATH."
   (directory-files-recursively path ".*\.pdf"))
 
 (defun annotation-contents (annot)
+  "Return the text content in ANNOT. Newlines are converted to
+spaces."
   (replace-regexp-in-string "\n" " " (cdr (assoc 'contents annot))))
 
 (defun get-all-annotations (file)
+  "Return all annotations saved in FILE, as a concatenation of
+their contents."
   (let* ((annots (with-current-buffer (find-file file)
                    (pdf-view-mode)
                    (pdf-annot-getannots nil nil))))
@@ -31,6 +36,10 @@
         nil))))
 
 (cl-defun pdf-search (pattern &optional (path "."))
+  "Search for PATTERN in the annotations for every PDF file
+recursively found starting from PATH. If PATTERN is found in a
+file, print its absolute filename and the entire set of
+annotations saved in the file."
   (message (format "Looking for \"%s\" in %s" pattern path))
   (dolist (pdf-file (all-pdf-files path))
     (search-in-annots pattern pdf-file)))
