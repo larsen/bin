3c04bca684a2cd8c0fb0c7e910cb47f712008bbc
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-05-30 16:39:19 +0200

    additional screen setup scripts

---
+++ t-display.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1080x608 --rotate normal --output eDP-1 --off --output DP-2 --off
