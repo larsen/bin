59f64e4879adf2d7a86ec25753e53fd62200288c
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-08-02 18:23:25 +0200

    kitty gnuplot integration

--- functions.sh
+++ functions.sh
@@ -55,3 +55,17 @@ function cache() {
 
   cat $cache_file
 }
+
+
+# kitty gnuplot integration
+function iplot {
+    cat <<EOF | gnuplot
+    set terminal pngcairo enhanced font 'Fira Sans,10'
+    set autoscale
+    set samples 1000
+    set output '|kitty +kitten icat --stdin yes'
+    set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb"#fdf6e3" behind
+    plot $@
+    set output '/dev/null'
+EOF
+}
