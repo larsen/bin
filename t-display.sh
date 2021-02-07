85729deea7b62e9a36e5bcdec56c19a14a4009db
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-02-07 14:00:34 +0100

    let's also reset xmodmap and wacom tablet while we're at it

--- t-display.sh
+++ t-display.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1080x608 --rotate normal --output eDP-1 --off --output DP-2 --off
+xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate right \
+  --output HDMI-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1080x608 --rotate normal \
+  --output eDP-1 --off --output DP-2 --off
+
+xmodmap ~/.xmodmap
+wacom-bamboo-setup
