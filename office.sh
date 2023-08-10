ba09cbc269311d77d8b442ac68c8820049a933ed
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-08-10 17:41:46 +0200

    updated office screen setup

--- office.sh
+++ office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --off --output DP-2 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 3067x840 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI-1 --off --output DP-2 --primary --mode 1920x1080 --pos 1080x652 --rotate normal --output HDMI-2 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
